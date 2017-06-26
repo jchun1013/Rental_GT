8854b31e0bb19d2d1f3e7e116cc24211e03c9254
jwchung1470 <pj6995@naver.com>
2017-06-26 18:44:21 -0400

    user class

--- Project_GT/app/src/main/java/com/example/joon1/project_gt/Model/User.java
+++ Project_GT/app/src/main/java/com/example/joon1/project_gt/Model/User.java
@@ -5,4 +5,39 @@ package com.example.joon1.project_gt.Model;
  */
 
 public class User {
+    private String username;
+    private String name;
+    private String password;
+    private String email;
+    private String phoneNumber;
+    private String gender;
+
+    public User(String username, String name, String password, String email, String phoneNumber, String gender) {
+        this.username = username;
+        this.name = name;
+        this.password = password;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.gender = gender;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getGender() {
+        return gender;
+    }
 }
